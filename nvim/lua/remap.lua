a5d7f975a06a4d215ac0c00892e6453e85b93d5f
kokopi-dev <km.commissions@gmail.com>
2025-09-30 14:53:40 +0900

    update:buf jump system

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -18,101 +18,133 @@ local tab_buffer_histories = {}
 local is_navigating = false
 
 local function get_current_tab_history()
-  local tab = vim.fn.tabpagenr()
-  if not tab_buffer_histories[tab] then
-    tab_buffer_histories[tab] = {
-      buffers = {},
-      index = 1
-    }
-  end
-  return tab_buffer_histories[tab]
+    local tab = vim.fn.tabpagenr()
+    if not tab_buffer_histories[tab] then
+        tab_buffer_histories[tab] = {
+            buffers = {},
+            index = 1
+        }
+    end
+    return tab_buffer_histories[tab]
 end
 
 vim.api.nvim_create_autocmd("BufEnter", {
-  callback = function()
-    if is_navigating then return end
-    local buf = vim.fn.bufnr('%')
-    if not vim.bo[buf].buflisted then return end
-    local history = get_current_tab_history()
-    -- If we're in the middle of history and switch to a new buffer,
-    -- truncate future history and add new buffer
-    if history.index < #history.buffers then
-      for i = #history.buffers, history.index + 1, -1 do
-        table.remove(history.buffers)
-      end
+    callback = function()
+        if is_navigating then return end
+        local buf = vim.fn.bufnr('%')
+        if not vim.bo[buf].buflisted then return end
+        local history = get_current_tab_history()
+
+        -- Remove any existing occurrence of this buffer from history
+        for i = #history.buffers, 1, -1 do
+            if history.buffers[i] == buf then
+                table.remove(history.buffers, i)
+                if i < history.index then
+                    history.index = history.index - 1
+                elseif i == history.index then
+                    history.index = math.max(1, history.index - 1)
+                end
+            end
+        end
+
+        -- If we're in the middle of history and switch to a new buffer,
+        -- truncate future history
+        if history.index < #history.buffers then
+            for i = #history.buffers, history.index + 1, -1 do
+                table.remove(history.buffers)
+            end
+        end
+
+        -- Add the buffer to history
+        table.insert(history.buffers, buf)
+        history.index = #history.buffers
     end
-    -- Don't add duplicate if it's already at current position
-    if history.buffers[history.index] ~= buf then
-      table.insert(history.buffers, buf)
-      history.index = #history.buffers
-    end
-  end
 })
 
 vim.keymap.set("n", "<A-b>", function()
-  local history = get_current_tab_history()
-  if history.index > 1 then
-    is_navigating = true
-    history.index = history.index - 1
-    vim.cmd('buffer ' .. history.buffers[history.index])
-    vim.schedule(function() is_navigating = false end)
-  end
+    local history = get_current_tab_history()
+    if history.index > 1 then
+        is_navigating = true
+        history.index = history.index - 1
+        local target_buf = history.buffers[history.index]
+
+        -- Check if buffer still exists and is valid
+        if vim.fn.bufexists(target_buf) == 1 and vim.bo[target_buf].buflisted then
+            vim.cmd('buffer ' .. target_buf)
+        else
+            -- Skip invalid buffers
+            table.remove(history.buffers, history.index)
+            history.index = math.max(1, history.index - 1)
+        end
+
+        vim.schedule(function() is_navigating = false end)
+    end
 end)
 
 vim.keymap.set("n", "<A-n>", function()
-  local history = get_current_tab_history()
-  if history.index < #history.buffers then
-    is_navigating = true
-    history.index = history.index + 1
-    vim.cmd('buffer ' .. history.buffers[history.index])
-    vim.schedule(function() is_navigating = false end)
-  end
+    local history = get_current_tab_history()
+    if history.index < #history.buffers then
+        is_navigating = true
+        history.index = history.index + 1
+        local target_buf = history.buffers[history.index]
+
+        -- Check if buffer still exists and is valid
+        if vim.fn.bufexists(target_buf) == 1 and vim.bo[target_buf].buflisted then
+            vim.cmd('buffer ' .. target_buf)
+        else
+            -- Skip invalid buffers
+            table.remove(history.buffers, history.index)
+            history.index = math.min(#history.buffers, history.index)
+        end
+
+        vim.schedule(function() is_navigating = false end)
+    end
 end)
 
 vim.keymap.set("n", "<A-c>", function()
-  local current_buf = vim.fn.bufnr('%')
-  local history = get_current_tab_history()
-  history.buffers = {current_buf}
-  history.index = 1
-  print("Buffer history cleared")
+    local current_buf = vim.fn.bufnr('%')
+    local history = get_current_tab_history()
+    history.buffers = { current_buf }
+    history.index = 1
+    print("Buffer history cleared")
 end)
 
 vim.keymap.set("n", "<A-q>", function()
-  local current_buf = vim.fn.bufnr('%')
-  local history = get_current_tab_history()
-  -- Remove current buffer from history
-  for i = #history.buffers, 1, -1 do
-    if history.buffers[i] == current_buf then
-      table.remove(history.buffers, i)
-      if i <= history.index then
-        history.index = math.max(1, history.index - 1)
-      end
+    local current_buf = vim.fn.bufnr('%')
+    local history = get_current_tab_history()
+    -- Remove current buffer from history
+    for i = #history.buffers, 1, -1 do
+        if history.buffers[i] == current_buf then
+            table.remove(history.buffers, i)
+            if i <= history.index then
+                history.index = math.max(1, history.index - 1)
+            end
+        end
     end
-  end
-  -- Navigate to previous buffer in history if available
-  if #history.buffers > 0 and history.index <= #history.buffers then
-    is_navigating = true
-    vim.cmd('buffer ' .. history.buffers[history.index])
-    vim.schedule(function() is_navigating = false end)
-  end
-  -- Delete the buffer
-  vim.cmd('bd ' .. current_buf)
+    -- Navigate to previous buffer in history if available
+    if #history.buffers > 0 and history.index <= #history.buffers then
+        is_navigating = true
+        vim.cmd('buffer ' .. history.buffers[history.index])
+        vim.schedule(function() is_navigating = false end)
+    end
+    -- Delete the buffer
+    vim.cmd('bd ' .. current_buf)
 end)
 
 -- Clean up history when a tab is closed
 vim.api.nvim_create_autocmd("TabClosed", {
-  callback = function()
-    local closed_tab = tonumber(vim.fn.expand("<afile>"))
-    if closed_tab then
-        tab_buffer_histories[closed_tab] = nil
+    callback = function()
+        local closed_tab = tonumber(vim.fn.expand("<afile>"))
+        if closed_tab then
+            tab_buffer_histories[closed_tab] = nil
+        end
     end
-  end
 })
 
 -- Makes shift+j not move cursor to the end
 vim.keymap.set("n", "J", "mzJ`z")
 -- collapse a tag with the cursor at the start of the tag
-vim.keymap.set("n", "<leader>j", "gJdw:j!<CR>", {silent=true})
+vim.keymap.set("n", "<leader>j", "gJdw:j!<CR>", { silent = true })
 
 -- nnoremap <silent> <C-l> :nohl<CR><C-l>
 -- vim.keymap.set("n", "<C-l>", vim.cmd(":nohl<CR><C-l>"))
