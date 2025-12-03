07b20af203b9bbf1e9188e241b676d567d34c6b3
kokopi <haruspace.dev@gmail.com>
2025-12-03 23:58:24 +0900

    add:wip mango

--- mango/mango/autostart.sh
+++ mango/mango/autostart.sh
@@ -9,10 +9,12 @@ swww clear 000000
 sleep 0.1
 swww img ~/pictures/flower.jpg --transition-type fade --transition-duration 1 &
 
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
-
-export WAYLAND_DISPLAY=wayland-1
+export WAYLAND_DISPLAY=${WAYLAND_DISPLAY:-wayland-1}
 export XDG_CURRENT_DESKTOP=wlroots
+
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
+/usr/lib/xdg-desktop-portal &
+
 export DISPLAY=:0
 
 wl-clip-persist --clipboard regular --reconnect-tries 0 &
@@ -22,3 +24,6 @@ gsettings set org.gnome.desktop.interface gtk-theme 'tokyonight-dark'
 gsettings set org.gnome.desktop.interface icon-theme 'catppuccin-mocha'
 gsettings set org.gnome.desktop.interface font-name 'Geist 11'
 
+fcitx5 -d
+
+kanshi &
---
+++ mango/update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp -r ~/.config/mango/* ./mango
+cp -r ~/.config/waybar/* ./waybar
+cp -r ~/.config/kanshi/* ./kanshi
+cp -r ~/.config/xdg-desktop-portal/* ./xdg-desktop-portal
