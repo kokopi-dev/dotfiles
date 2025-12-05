3462a4d1e6b6b518850a9254f5e89956146793c6
kokopi <haruspace.dev@gmail.com>
2025-12-06 00:06:31 +0900

    update:save path

--- mango/local-bin/ss.sh
+++ mango/local-bin/ss.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grim -g "$(slurp)" - | satty --filename - --output-filename ~/pictures/$(date +"%Y-%m-%d_%H-%M-%S").png --early-exit --copy-command wl-copy
+grim -g "$(slurp)" - | satty --filename - --output-filename ~/pictures/ss/$(date +"%Y-%m-%d_%H-%M-%S").png --early-exit --copy-command wl-copy
