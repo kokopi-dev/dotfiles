b794bce0ea8e6547ff0d8a0c6ea29acf052740ff
kokopi <haruspace.dev@gmail.com>
2025-12-11 23:53:28 +0900

    update:script

--- mango/local-bin/sc.sh
+++ mango/local-bin/sc.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 if pgrep -x wl-screenrec > /dev/null; then pkill -INT wl-screenrec && notify-send "🎥 Recording Stopped" "Saved to ~/videos"; else GEOMETRY=$(slurp); if [ -n "$GEOMETRY" ]; then notify-send "🎥 Recording Started" "Press Super+Alt+v to stop" && wl-screenrec -g "$GEOMETRY" -f ~/videos/recording-$(date +"%Y-%m-%d_%H-%M-%S").mp4 & fi; fi
+# silent portion record
+# if pgrep -x wl-screenrec > /dev/null; then pkill -INT wl-screenrec && notify-send "🎥 Recording Stopped" "Saved to ~/videos"; else GEOMETRY=$(slurp); if [ -n "$GEOMETRY" ]; then wl-screenrec -g "$GEOMETRY" -f ~/videos/recording-$(date +"%Y-%m-%d_%H-%M-%S").mp4 & fi; fi
+#
+# main screen only for yt
+# if pgrep -x wl-screenrec > /dev/null; then pkill -INT wl-screenrec && notify-send "🎥 Recording Stopped" "Saved to ~/videos"; else wl-screenrec -o DP-3 -f ~/videos/recording-$(date +"%Y-%m-%d_%H-%M-%S").mp4 & fi
