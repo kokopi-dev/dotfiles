49881893bb138bafd546cdb39a0ec5a2e258bd5b
kokopi-dev <km.commissions@gmail.com>
2026-03-18 17:26:55 +0900

    add:screenshot direct screen capture

--- mango/local-bin/ss.sh
+++ mango/local-bin/ss.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-grim -g "$(slurp)" - | satty --filename - --output-filename ~/pictures/ss/$(date +"%Y-%m-%d_%H-%M-%S").png --early-exit --copy-command wl-copy
+
+MODE=${1:-area}
+OUTPUT=~/pictures/$(date +"%Y-%m-%d_%H-%M-%S").png
+
+case "$MODE" in
+  area)
+    grim -g "$(slurp)" - | satty --filename - --output-filename "$OUTPUT" --early-exit --copy-command wl-copy
+    ;;
+  screen)
+    grim -g "$(slurp -o)" - | satty --filename - --output-filename "$OUTPUT" --early-exit --copy-command wl-copy
+    ;;
+  *)
+    echo "Usage: $0 [area|screen]"
+    exit 1
+    ;;
+esac
