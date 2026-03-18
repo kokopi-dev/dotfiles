b5a48ae7411d3db7f377115d79910b7c3038175f
kokopi-dev <km.commissions@gmail.com>
2026-03-18 17:27:21 +0900

    add:supermaven

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,15 +4,18 @@ require("settings")
 
 -- lazy package manager setup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -181,6 +184,12 @@ require("lazy").setup({
                 highlight = { enable = true },
                 indent = { enable = true },
             })
+        end,
+    },
+    {
+        "supermaven-inc/supermaven-nvim",
+        config = function()
+            require("supermaven-nvim").setup({})
         end,
     },
     -- lsp
@@ -444,7 +453,7 @@ vim.lsp.config('tailwindcss', {
             },
         },
     },
-    filetypes = { "htmldjango", "templ" },
+    filetypes = { "htmldjango", "templ", 'html', 'css', 'javascript', 'typescript', 'jsx', 'tsx' },
 })
 vim.lsp.enable('tailwindcss')
 
@@ -597,6 +606,7 @@ cmp.setup({
         end, { "i", "s" }),
     },
     sources = {
+        { name = "supermaven" },
         { name = "nvim_lsp" },
         { name = "luasnip" },
         { name = "buffer" },
