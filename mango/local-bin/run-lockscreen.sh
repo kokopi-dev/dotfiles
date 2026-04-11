2e5328884afdcbc1d8909e7601d8bcda5461924d
kokopi-dev <derrick@astra-rei.com>
2026-04-11 22:39:28 +0900

    update:lockscreen run

--- mango/local-bin/run-lockscreen.sh
+++ mango/local-bin/run-lockscreen.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-swaylock -i ~/pictures/flower.jpg --effect-blur 7x5 --effect-vignette 0.5:0.5 --clock --indicator --ring-color E8D4A0FF --key-hl-color F4A261FF --bs-hl-color EE9B00FF --inside-color 00000088 --text-color FFFFFFFF --line-color 00000000 --separator-color 00000000
+LOCK="$HOME/.local/share/quickshell-lockscreen/lock.sh"
+bash "$LOCK"
+# swaylock -i ~/pictures/flower.jpg --effect-blur 7x5 --effect-vignette 0.5:0.5 --clock --indicator --ring-color E8D4A0FF --key-hl-color F4A261FF --bs-hl-color EE9B00FF --inside-color 00000088 --text-color FFFFFFFF --line-color 00000000 --separator-color 00000000
